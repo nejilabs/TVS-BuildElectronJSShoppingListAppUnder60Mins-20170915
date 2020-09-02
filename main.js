8ee62edd9791208ea059faa2f5b14b89dc457e20
nellyXinwei <nelsonalbajr@yahoo.com>
2020-09-02 16:01:57 +0800

    added handle function and template for create add new window feature

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ path = require('path');
 const {app,BrowserWindow, Menu} = electron;
 
 let mainWindow;
+let addWindow;
+
 
 //Listen for app to be ready
 app.on('ready',function (){
@@ -24,12 +26,33 @@ app.on('ready',function (){
 })
 
 
+//HANDLE CREATE ADD WINDOW
+function createAddWindow(){
+  //create new Window
+  addWindow = new BrowserWindow({
+    width:200,
+    height:300,
+    title:'Add Shopping List Item'
+  });
+  //load html into window
+  addWindow.loadURL(url.format({
+    pathname:path.join(__dirname, 'addWindow.html'),
+    protocol:'file',
+    slashes:true
+  }));
+}
+
+
+
 //Create Menu Template
 const mainMenuTemplate = [
   {
     label:'file',
     submenu:[
-      {label:'Add Item'},
+      {
+        label:'Add Item',
+        click(){createAddWindow()}
+      },
       {label:'Clear Items'},
       {
         label:'Quit',
